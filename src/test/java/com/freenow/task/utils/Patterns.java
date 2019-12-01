4a18dc50c7763233962cceafa7f4b51f2e485235
Mykola Grybyk <mykola.grybyk@gmail.com>
2019-12-01 20:04:49 +0100

    add unit test for email pattern

--- src/test/java/com/freenow/task/utils/Patterns.java
+++ src/test/java/com/freenow/task/utils/Patterns.java
@@ -1,48 +1,52 @@
 package com.freenow.task.utils;
 
-import org.hamcrest.core.Every;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.matchesPattern;
 import static org.hamcrest.Matchers.not;
 
-public abstract class Patterns {
+public class Patterns {
     // todo handle duplicate symbols
     private static final String emailPattern =
             "^(?![._\\-+])[a-zA-Z0-9_.+-]*[a-zA-Z0-9]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]*[a-zA-Z0-9]+$";
 
     public static Pattern getEmailPattern() {
-        // Patterns.verifyEmailPattern();
         return Pattern.compile(emailPattern);
     }
 
-    @SuppressWarnings("unused")
-    private static void verifyEmailPattern() {
-        var validEmails = new ArrayList<String>() {{
-            add("q@q.q");
-            add("Q.q_w-w+1@foo1-bar2.foo.bar");
-        }};
-        var invalidEmails = new ArrayList<String>() {{
-            add(null);
-            add("");
-            add("@.");
-            add("q@.q");
-            add("@q.q");
-            add("q@");
-            add("q@q.");
-            add(".@q.q");
-            add("_@q.q");
-            add("-@q.q");
-            add("+@q.q");
-            add("q@q+q.q");
-            add("q@q_q.q");
-            add("q@q@q.q");
-            add("q@@q.q");
-        }};
-        assertThat(validEmails, Every.everyItem(matchesPattern(emailPattern)));
-        assertThat(invalidEmails, Every.everyItem(not(matchesPattern(emailPattern))));
+    @Tag("unit")
+    @ParameterizedTest
+    @ValueSource(strings = {"q@q.q", "Q.q_w-w+1@foo1-bar2.foo.bar"})
+    void validEmailTest(String email) {
+        assertThat(email, matchesPattern(emailPattern));
+    }
+
+    @Tag("unit")
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(strings = {
+            "",
+            "@.",
+            "q@.q",
+            "@q.q",
+            "q@",
+            "q@q.",
+            ".@q.q",
+            "_@q.q",
+            "-@q.q",
+            "+@q.q",
+            "q@q+q.q",
+            "q@q_q.q",
+            "q@q@q.q",
+            "q@@q.q",
+    })
+    void invalidEmailTest(String email) {
+        assertThat(email, not(matchesPattern(emailPattern)));
     }
 }
